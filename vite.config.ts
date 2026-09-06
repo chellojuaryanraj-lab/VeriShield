e7584c80df4b5a9ae84d5711c53200e1dd3aa2da
chellojuaryanraj-lab <chellojuaryanraj@gmail.com>
2026-09-06 20:08:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,3 +20,10 @@ export default defineConfig(() => {
     },
   };
 });
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react
+export default defineconfig(
+{
+  base: '/VerSheild/',
+  plugins: [react()],
+  })
